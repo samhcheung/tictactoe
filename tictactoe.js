b6db79f11849a69006dc58890ea362fcb2f61f8a
samhcheung <samhcheung@gmail.com>
2016-10-18 12:35:17 -0700

    Added prompts

--- tictactoe.js
+++ tictactoe.js
@@ -8,13 +8,16 @@ var Board = function() {
 }
 
 Board.prototype.displayBoard = function() {
+    console.log('    |' + '0' + '|' + '1' + '|' + '2' + '| Column');
   for(let i = 0; i < this.space.length; i++) {
-      console.log('|' + this.space[i][0] + '|' + this.space[i][1] + '|' + this.space[i][2] + '|');
+      console.log('Row' + i + '|' + this.space[i][0] + '|' + this.space[i][1] + '|' + this.space[i][2] + '|');
   }
 }
 
 Board.prototype.placePiece = function(row,column,type) {
-
+  if(type === 'x' || type === 'o'){
+    this.space[row][column] = type;
+  }
 }
 
 Board.prototype.checkWinner = function() {
@@ -22,26 +25,31 @@ Board.prototype.checkWinner = function() {
 }
 
 Board.prototype.doTurn = function() {
-  console.log(this.currentplayer + "'s turn");
-  prompt.start();
-  this.winner = 1;
-  // prompt.get(['row', 'column'], function (err, result) {
-  //     if (err) { return onErr(err); }
-  //     console.log('Command-line input received:');
-  //     console.log('  Username: ' + result.username);
-  //     console.log('  Email: ' + result.email);
-  //   });
-
-  //   function onErr(err) {
-  //     console.log(err);
-  //     return 1;
-  //   }
-
 }
 
 console.log("Hello welcome to tic tac toe");
 var a = new Board();
 a.displayBoard();
-while(a.winner === null) {
-  a.doTurn();
-}
+    function onErr(err) {
+      console.log(err);
+      return 1;
+    }
+  console.log(a.currentplayer + "'s turn");
+  prompt.start();
+  // this.winner = 1;
+  prompt.get(['row', 'column'], function (err, result) {
+      if (err) { return onErr(err); }
+      console.log('Command-line input received:');
+      console.log('  row: ' + result.row);
+      console.log('  column: ' + result.column);
+      
+    });
+
+
+
+// prompt.get(['row', 'column'], function (err, result) {
+//       if (err) { return onErr(err); }
+//       console.log('Command-line input received:');
+//       console.log('  row: ' + result.row);
+//       console.log('  column: ' + result.column);
+//     });
